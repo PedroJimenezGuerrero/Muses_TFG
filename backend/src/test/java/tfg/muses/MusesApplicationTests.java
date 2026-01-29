6d48f0b0bde8e35a2288663255fd8e16c3a46819
PedroJimenezGuerrero <pedrojimenezguerrero0@gmail.com>
2026-01-29 12:14:54 +0000

    chore: :tada: iniciar proyectos en nextjs y spring boot

---
+++ backend/src/test/java/tfg/muses/MusesApplicationTests.java
@@ -0,0 +1,13 @@
+package tfg.muses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
