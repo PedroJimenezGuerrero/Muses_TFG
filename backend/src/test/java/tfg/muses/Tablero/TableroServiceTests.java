f690e3cfaeec84639b7bd1cd34f9d844da46d1e2
PedroJimenezGuerrero <pedrojimenezguerrero0@gmail.com>
2026-02-02 12:23:53 +0000

    test(Tablero): :test_tube: añadir test fallido de TableroService #2

---
+++ backend/src/test/java/tfg/muses/Tablero/TableroServiceTests.java
@@ -0,0 +1,45 @@
+package tfg.muses.Tablero;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import tfg.muses.tablero.TableroModel;
+import tfg.muses.tablero.TableroService;
+
+public class TableroServiceTests {
+    private TableroService tableroService;
+    private TableroModel tablero;
+
+    @BeforeEach
+    public void setUp() {
+        tableroService = new TableroService();
+        tablero = new TableroModel();
+    }
+
+    @Test
+    public void testRotarAstrosWithValidPositions() {
+        tablero.setSolPos(1); // Sol: arriba centro
+        tablero.setLunaPos(5);  // Luna: Abajo centro
+
+        tableroService.rotarAstros(tablero);
+        
+        assertEquals(2, tablero.getSolPos());
+        assertEquals(6, tablero.getLunaPos());
+
+        tablero.setSolPos(3); // Sol: derecha centro
+        tablero.setLunaPos(7);  // Luna: Izquierda centro
+
+        tableroService.rotarAstros(tablero);
+
+        assertEquals(4, tablero.getSolPos());
+        assertEquals(0, tablero.getLunaPos());
+    }
+
+}
