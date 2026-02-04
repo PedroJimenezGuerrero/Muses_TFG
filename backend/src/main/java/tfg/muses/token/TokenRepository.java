e7bdce336b95507eaf2a1ac1b2e99ddc372e950a
PedroJimenezGuerrero <pedrojimenezguerrero0@gmail.com>
2026-02-04 18:36:25 +0000

    feat: :sparkles: Crear repositorio de las entidades Carta, Estadísticas, Musa, Token, Usuario
    #7

---
+++ backend/src/main/java/tfg/muses/token/TokenRepository.java
@@ -0,0 +1,7 @@
+package tfg.muses.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+}
---
+++ backend/src/main/java/tfg/muses/usuario/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package tfg.muses.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
