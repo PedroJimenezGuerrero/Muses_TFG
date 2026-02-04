2e9f48fd7147ee4f0d335b83d479bb359afbee1a
PedroJimenezGuerrero <pedrojimenezguerrero0@gmail.com>
2026-02-04 18:01:59 +0000

    feat: :sparkles: crear controlador de tablero
    #6

---
+++ backend/src/main/java/tfg/muses/tablero/TableroController.java
@@ -0,0 +1,71 @@
+package tfg.muses.tablero;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+
+
+@RestController
+@RequestMapping("tablero")
+public class TableroController {
+
+    @Autowired
+    private TableroService tableroService;
+
+    @PutMapping("rotarAstros/{id}")
+    public Tablero rotarAstros(@PathVariable Long id) {
+        Tablero tablero = tableroService.getById(id);
+        return tableroService.rotarAstros(tablero);
+    }
+
+    @PutMapping("revolucionSolar/{id}")
+    public Tablero revolucionSolar(@PathVariable Long id) {
+        Tablero tablero = tableroService.getById(id);
+        try {
+            tablero = tableroService.revolucionSolar(tablero);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tablero;
+    }
+
+    @PutMapping("revolucionLunar/{id}")
+    public Tablero revolucionLunar(@PathVariable Long id) {
+        Tablero tablero = tableroService.getById(id);
+        try {
+            tablero = tableroService.revolucionLunar(tablero);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tablero;
+    }
+
+    @GetMapping("{id}")
+    public Tablero getById(@RequestParam Long id) {
+        Tablero tablero = tableroService.getById(id);
+        return tablero;
+    }
+
+    @GetMapping("/byPartida/{partidaId}")
+    public Tablero getByPartidaId(@RequestParam Long partidaId) {
+        Tablero tablero = tableroService.getByPartidaId(partidaId);
+        return tablero;
+    }
+    
+    @PostMapping("")
+    public Tablero createTablero(Tablero tablero) {
+        Tablero newTablero = tableroService.save(tablero);        
+        return newTablero;
+    }
+    
+    
+    
+}
---
+++ docs/Monitoreo/Devlogs/Devlog5-04_02.md
@@ -0,0 +1,4 @@
+# Devlog 5: 04/02/2026
+
+- Como ayer se terminaron las features que aparecen en el diccionario de la EDT, hoy se creará la estructura MVC del backend
+- 
