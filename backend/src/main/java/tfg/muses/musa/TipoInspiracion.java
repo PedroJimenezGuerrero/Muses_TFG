1e44d821aca8d30f3df59ba1b75702a5f3b8ee85
PedroJimenezGuerrero <pedrojimenezguerrero0@gmail.com>
2026-02-24 11:54:56 +0000

    refactor(Cartas): :recycle: refactorizar TipoMusa para añadir TIpoInspiración según la musa
    #12

---
+++ backend/src/main/java/tfg/muses/musa/TipoInspiracion.java
@@ -0,0 +1,6 @@
+package tfg.muses.musa;
+
+public enum TipoInspiracion {
+    LADOS,
+    VERTICES;
+}
--- backend/src/main/java/tfg/muses/musa/TipoMusa.java
+++ backend/src/main/java/tfg/muses/musa/TipoMusa.java
@@ -2,24 +2,26 @@ package tfg.muses.musa;
 
 public enum TipoMusa {
     // Puntos por nivel (del primer al tercer puesto)
-    CLIO(7, 5, 3),
-    EUTERPE(7, 6, 5),
-    TALIA(8, 6, 4),
-    MELPOMENE(9, 6, 3),
-    TERPSICORE(5, 4, 3),
-    ERATO(7, 4, 1),
-    POLIMNIA(6, 5, 4),
-    URANIA(6, 4, 2),
-    CALIOPE(8, 5, 2);
+    CLIO(7, 5, 3, TipoInspiracion.LADOS),
+    EUTERPE(7, 6, 5, TipoInspiracion.LADOS),
+    TALIA(8, 6, 4, TipoInspiracion.LADOS),
+    MELPOMENE(9, 6, 3, TipoInspiracion.LADOS),
+    TERPSICORE(5, 4, 3, TipoInspiracion.VERTICES),
+    ERATO(7, 4, 1, TipoInspiracion.VERTICES),
+    POLIMNIA(6, 5, 4, TipoInspiracion.VERTICES),
+    URANIA(6, 4, 2, TipoInspiracion.VERTICES),
+    CALIOPE(8, 5, 2, TipoInspiracion.LADOS);
 
     private final int nivel1;
     private final int nivel2;
     private final int nivel3;
+    private final TipoInspiracion tipoInspiracion;
 
-    TipoMusa(int n1, int n2, int n3) {
+    TipoMusa(int n1, int n2, int n3, TipoInspiracion tipoInspiracion) {
         this.nivel1 = n1;
         this.nivel2 = n2;
         this.nivel3 = n3;
+        this.tipoInspiracion = tipoInspiracion;
     }
 
     public int getPuntos(int nivel) {
@@ -29,5 +31,9 @@ public enum TipoMusa {
             case 3 -> nivel3;
             default -> throw new IllegalArgumentException("Nivel inválido: " + nivel);
         };
+    }
+
+    public TipoInspiracion getTipoInspiracion() {
+        return tipoInspiracion;
     }
 }
